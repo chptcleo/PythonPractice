846307d07993b110659cbf07893caa45031559f7
walchen <wallace.chen@nokia-sbell.com>
2018-04-03 20:33:25 +0800

    upate

---
+++ com/bit_operation/or.py
@@ -0,0 +1,5 @@
+x=2|4
+print x
+if True|4:
+    print 'inside'
+print 3|5
---
+++ com/string/string_reverse.py
@@ -0,0 +1,16 @@
+# print string.count('abc','c')
+my_str = "abcde"
+print my_str.find("c")
+str_size = len(my_str)
+# print my_str.find("e")
+list = []
+for i in range(str_size):
+    reversed_i = str_size - i - 1
+    list.append(my_str[reversed_i])
+
+print "".join(list)
+my_str.replace('a', 'x')
+print my_str
+delimiter = ','
+mylist = ['Brazil', 'Russia', 'India', 'China']
+print delimiter.join(mylist)
