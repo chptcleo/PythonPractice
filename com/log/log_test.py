49a9894757d843eb2f4a3323becd6a08ee3c06bf
walchen <wallace.chen@nokia-sbell.com>
2018-04-09 20:44:14 +0800

    update

--- com/log/log_test.py
+++ com/log/log_test.py
@@ -7,6 +7,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, filename='mylog.log')
     logging.info('Start program')
     logging.debug('1 divide 0')
-    1/0
     logging.info('After divide')
     logging.info('End program')
